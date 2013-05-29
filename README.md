b47dd2de7871ab647cc2f1f62c1db2350b0a1395
Marcin N-K (buttitch) <marcin.nk@gmail.com>
2013-05-29 23:23:41 +0930

    Refactored to allow CLI control and removed hardcodings.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-datagrub
+DataGrub
 ========
 
 A grub that eats data!
 
 Running
-========
+--------
 
 On *nix systems:
 
@@ -13,3 +13,29 @@ On *nix systems:
 on others:
 
 groovy DataGrub.groovy [args]
+
+Usage
+--------
+
+`
+usage: DataGrub.groovy [arguments]
+
+ -d,--download            Download files that match the specified "tags"
+                          and "formats" filter, if this option is not
+                          specified the DataGrub just prints what files
+                          match the specified "tags" and "formats" filter
+                          to standard output.
+ -f,--formats <formats>   What file formats are we after, defined by
+                          "formats" comma separated list
+ -h,--help                Show usage information
+ -l,--list                list all available tags
+ -o,--output <dir>        output directory to be used for downloaded
+                          files, defined by "dir"
+ -t,--tags <tags>         What datasets should be downloaded, defined by
+                          "tags" comma separated list                          
+`
+
+Examples
+--------
+
+later
--- src/buttitch/datagrub/DataGrub.groovy
+++ src/buttitch/datagrub/DataGrub.groovy
@@ -1,55 +1,131 @@
 #!/usr/bin/env groovy
 
-/**
- * Quick and nasty script for grabbing data from data.sa.gov.au
- *
- * @author Marcin N-K (@buttitch)
- */
-
 package buttitch.datagrub
 
 import java.text.SimpleDateFormat
 import groovyx.net.http.*
 @Grab(group='org.codehaus.groovy.modules.http-builder',
-	module='http-builder', version='0.5.2' )
+module='http-builder', version='0.5.2' )
 
-// TODO make generic - for now hardcoded to data.sa.gov.au
-def base = "http://data.sa.gov.au/api/3/action/"
-def isoTimestamp = new SimpleDateFormat("yyyyMMdd'T'HHmmss").format(new Date())
- 
-def http = new HTTPBuilder(base)
- 
-http.get(path: 'tag_show',
-	 query: [id: 'planning']) { response, json ->
- 
-	if (response.status == 200 && json.success) {
-		json.result.packages.each { pack ->
-			// TODO make generic - for now just download any resources in shapefile format
-			pack.resources.each { resource ->
-				if (resource.format.toLowerCase() == "shp") {
-					download(resource.url)
+/**
+ * Quick and nasty script for grabbing data from data.sa.gov.au
+ * 
+ * TODO make generic - for now hardcoded to data.sa.gov.au
+ * @author Marcin N-K (@buttitch)
+ */
+class DataGrub { 
+	def base = "http://data.sa.gov.au/api/3/action/"
+	def http = new HTTPBuilder(base)
+	def config = [:]
+
+	def DataGrub() {}
+	
+	def DataGrub(config) {
+		this.config = config
+	}
+
+	def download() {
+		download(config)
+	}
+
+	def download(config) {
+		config?.tags.each { tag ->
+			http.get(path: 'tag_show',
+			         query: [id: tag]) { response, json ->
+				if (response.status == 200 && json.success) {
+					json.result.packages.each { pack ->
+						pack.resources.each { resource ->
+							if (!config.formats || config.formats.contains(resource.format.toLowerCase())) {
+								if (config.download) {
+									downloadFile(resource.url, config)
+								} else {
+									println resource.toString(2)
+								}
+							}
+						}
+					}
+				} else {
+					System.err.println("Request failed. HTTP: ${response.status} Error: ${json && json.error}")
 				}
 			}
 		}
-		
-		// TODO remove this debug - for now dump json to file for eyeballing
-		def dump = new File("query-${isoTimestamp}.json")
-		dump << json.toString(2)
-	} else {
-		System.err.println("Request failed. HTTP: ${response.status} Error: ${json && json.error}")
 	}
-}
+	
+	def list() {
+		http.get(path: 'tag_list') { response, json ->
+			if (response.status == 200 && json.success) {
+				println json.result.toString(2)
+			}
+		}
+	}
+
+	
+	//---
+	// helper methods
+	//---
+
+	def downloadFile(address, config) {
+		print "Downloading: ${address} "
+		def file = new FileOutputStream(new File(getOutputDir(config), address.tokenize("/")[-1]))
+		def out = new BufferedOutputStream(file)
+		out << new URL(address).openStream()
+		out.close()
+		println "DONE"
+	}
+	
+	def getOutputDir(config) {
+		def dir = new File(config.output ?: ".")
+		dir.mkdirs()
+		return dir
+	}
+	
+	
+	//---
+	// Main
+	//---
+
+	static main(args) {
+		// command-line args
+		def cli = new CliBuilder(usage: 'DataGrub.groovy [arguments]')
+		cli.with {
+			h longOpt: 'help', 'Show usage information'
+			f longOpt: 'formats', args: 1, argName: 'formats', 'What file formats are we after, defined by "formats" comma separated list'
+			l longOpt: 'list',   'list all available tags'
+			o longOpt: 'output', args: 1, argName: 'dir', 'output directory to be used for downloaded files, defined by "dir"'
+			t longOpt: 'tags',  args: 1, argName: 'tags', 'What datasets should be downloaded, defined by "tags" comma separated list'
+			d longOpt: 'download', 'Download files that match the specified "tags" and "formats" filter, if this option is not specified the DataGrub just prints what files match the specified "tags" and "formats" filter to standard output.' 
+		}
+		def options = cli.parse(args)
+		if (!options || options.h) {
+			cli.usage()
+			return
+		}
+
+		// determine config
+		def config = [download: false]
+		if (options.formats) {  
+			def formats = options.formats.toLowerCase().split(/\s*,\s*/)?.toList().toSet()
+			config.put('formats', formats)
+		}  
+		if (options.output) {  
+			config.put('output', options.output)
+		}
+		if (options.tags) {
+			def tags = options.tags.split(/\s*,\s*/)?.toList()
+			config.put('tags', tags)
+		}
+		if (options.download) {
+			config.put('download', true)
+		}
 		
-//---
-// helper functions
-//---
+		println "DataGrub - Using config: ${config}"
 		
-def download(address) {
-	print "Downloading: ${address} "
-	def file = new FileOutputStream(address.tokenize("/")[-1])
-	def out = new BufferedOutputStream(file)
-	out << new URL(address).openStream()
-	out.close()
-	println "DONE"
+		// do the work
+		def grub = new DataGrub(config)
+		if (options.list) {
+			grub.list()
+		}
+		grub.download()
+	}
 }
 
